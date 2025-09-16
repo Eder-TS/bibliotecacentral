c9a870438f116f4d12944856430108341c7bd48e
Eder-TS <steder20@yahoo.com.br>
2025-09-16 20:26:20 -0300

    Fix: get livros

--- functions/livros.js
+++ functions/livros.js
@@ -3,25 +3,25 @@ import serverless from 'serverless-http';
 import livroControllers from '../src/controllers/livroControllers.js';
 import cors from 'cors';
 
-const app = express();
+const livros = express();
 
-app.use(express.json());
+livros.use(express.json());
 
-app.use(cors());
+livros.use(cors());
 
 const router = express.Router();
 
 router.get('/', livroControllers.buscaTodosLivrosController);
 
-app.use('/.netlify/functions/livros', router);
+livros.use('/.netlify/functions/livros', router);
 
-app.use((req, res) => {
+livros.use((req, res) => {
   res.status(404).send({
     mensagem: `Rota ${req.method} ${req.originalUrl} não encontrada.`,
   });
 });
 
-const lambda = serverless(app);
+const lambda = serverless(livros);
 
 export async function handler(event, context) {
   return await lambda(event, context);
