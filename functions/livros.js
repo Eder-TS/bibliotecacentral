ad334ad0a30a9f8b4d21c067eeb26b539a125d83
Eder-TS <steder20@yahoo.com.br>
2025-09-15 22:42:50 -0300

    Feat: get livros

---
+++ functions/livros.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import serverless from 'serverless-http';
+import livroControllers from '../src/controllers/livroControllers';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const router = express.Router();
+
+router.get('/livros', livroControllers.buscaTodosLivrosController);
+
+app.use('/.netlify/functions/app', router);
+
+app.use((req, res) => {
+  res.status(404).send({
+    mensagem: `Rota ${req.method} ${req.originalUrl} não encontrada.`,
+  });
+});
+
+const lambda = serverless(app);
+
+export async function handler(event, context) {
+  return await lambda(event, context);
+}
